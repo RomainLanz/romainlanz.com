4e01794ff52b4a4adb231a24bd39b396586a7dfe
Romain Lanz <romain.lanz+github@pm.me>
2023-10-16 11:56:58 +0200

    chore: add hotwire turbo

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,8 @@
 import '../css/app.scss'
+import * as Turbo from '@hotwired/turbo'
 
+// Import all images and fonts
 import.meta.glob(['../fonts/**'])
 import.meta.glob(['../images/**'])
+
+Turbo.start()
