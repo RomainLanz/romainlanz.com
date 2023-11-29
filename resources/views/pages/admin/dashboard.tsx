479c6f4b3bb923c6745d1263202d0d27ee5e38e0
Romain Lanz <romain.lanz+github@pm.me>
2023-11-29 21:16:33 +0100

    feat(admin): make the dashboard pretty

--- resources/views/pages/admin/dashboard.tsx
+++ resources/views/pages/admin/dashboard.tsx
@@ -4,15 +4,25 @@ import { route } from '#start/view'
 
 export function Dashboard() {
   return (
-    <App title="Dashboard">
+    <App title="Dashboard" page="dashboard">
       <MaxWidthWrapper>
-        <aside>
-          <ul>
-            <li>
-              <a href={route('admin.blog.posts.create')}>Post Create</a>
-            </li>
-          </ul>
-        </aside>
+        <>
+          <h1 class="page_title">Dashboard</h1>
+          <div class="dashboard_wrapper">
+            <aside class="card">
+              <ul>
+                <li>
+                  <a href={route('admin.blog.posts.create')}>Articles</a>
+                </li>
+                <li>
+                  <a href={route('admin.redirects.index')}>Redirection</a>
+                </li>
+              </ul>
+            </aside>
+
+            <main class="card"></main>
+          </div>
+        </>
       </MaxWidthWrapper>
     </App>
   )
