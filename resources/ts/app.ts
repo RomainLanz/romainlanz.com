4e8abb28a4d9fc5cd06a869231d7f0cfc9187ae4
Romain Lanz <romain.lanz+github@pm.me>
2024-03-09 19:55:23 +0100

    feat: update default unpoly configuration

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -3,3 +3,12 @@ import 'unpoly'
 // Import all images and fonts -- Needed for Vite
 import.meta.glob(['../fonts/**'])
 import.meta.glob(['../images/**'])
+
+// Follows all links by default
+up.link.config.followSelectors.push('a[href]')
+
+// Preloads all links by default
+up.link.config.preloadSelectors.push('a[href]')
+
+// Handle all forms by default
+up.form.config.submitSelectors.push(['form'])
