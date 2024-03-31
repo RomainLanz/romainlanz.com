e4ac1f33feadcc72e76b54f3e225e7b3b99191b2
Romain Lanz <romain.lanz@pm.me>
2024-04-01 00:11:33 +0200

    chore: filter http logs

--- start/events.ts
+++ start/events.ts
@@ -1,8 +1,19 @@
+import app from '@adonisjs/core/services/app'
 import string from '@adonisjs/core/helpers/string'
 import logger from '@adonisjs/core/services/logger'
 import emitter from '@adonisjs/core/services/emitter'
 
+const blacklistedUrls = ['/node_modules', '/resources', '/@']
+
 emitter.on('http:request_completed', function ({ ctx, duration }) {
+  if (blacklistedUrls.some((url) => ctx.request.url().startsWith(url))) {
+    return
+  }
+
+  if (!app.inDev) {
+    return
+  }
+
   logger.info(
     `[HTTP] ${ctx.response.getStatus()} - ${ctx.request.method()} ${ctx.request.url()} - ${string.prettyHrTime(duration)}`
   )
