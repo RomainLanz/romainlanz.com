37063bcc34b09e2eedd5d4f117e0c38d82bfe2c1
Romain Lanz <romain.lanz@pm.me>
2024-06-11 19:43:26 +0200

    fix: correct path to glob import assets

--- resources/ts/vendors/vite.ts
+++ resources/ts/vendors/vite.ts
@@ -1,3 +1,3 @@
 // Import all images and fonts -- Needed for Vite
-import.meta.glob(['../fonts/**'])
-import.meta.glob(['../images/**'])
+import.meta.glob(['../../fonts/**'])
+import.meta.glob(['../../images/**'])
