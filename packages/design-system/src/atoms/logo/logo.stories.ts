27578261b1ed3fb86988f32f0b348f50c926d2d1
Romain Lanz <romain.lanz@pm.me>
2024-07-31 21:28:07 +0200

    feat(design-system:atoms): add logo component

---
+++ packages/design-system/src/atoms/logo/logo.stories.ts
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+import Logo from "./logo.vue";
+
+const meta = {
+  component: Logo,
+  title: "Atoms/Logo",
+} satisfies Meta<typeof Logo>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Colorful: Story = {
+  argTypes: {
+    monochrome: {
+      type: "boolean",
+    },
+  },
+  args: {
+    monochrome: false,
+  },
+};
+
+export const Monochrome: Story = {
+  argTypes: Colorful.argTypes,
+  args: {
+    monochrome: true,
+  },
+};
