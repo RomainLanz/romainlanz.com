6f24b158d09db17480a6a67a833212347a74e02e
Romain Lanz <romain.lanz@pm.me>
2024-09-08 00:31:13 +0200

    refactor(design-system:tag): use unocss

--- packages/design-system/src/atoms/tag/tag.vue
+++ packages/design-system/src/atoms/tag/tag.vue
@@ -1,90 +1,56 @@
 <script lang="ts">
-import { cva, type VariantProps } from "class-variance-authority";
+	import { tv, type VariantProps } from 'tailwind-variants';
 
-const tag = cva("tag", {
-  variants: {
-    color: {
-      cyan: "cyan",
-      violet: "violet",
-      red: "red",
-      yellow: "yellow",
-      lime: "lime",
-    },
-  },
-});
+	const tag = tv({
+		base: 'bg-white border border-solid border-gray-800 rounded-ms text-gray-800 tracking-wider text-xs uppercase py-1.5 px-2',
 
-type TagProps = VariantProps<typeof tag>;
+		variants: {
+			color: {
+				cyan: 'bg-cyan-300',
+				violet: 'bg-violet-300',
+				red: 'bg-red-300',
+				yellow: 'bg-yellow-300',
+				lime: 'bg-lime-300',
+			},
+
+			type: {
+				span: '',
+				link: 'cursor-pointer hover:shadow-tiny transition-shadow duration-200 transition-ease-in-out',
+			},
+		},
+
+		defaultVariants: {
+			type: 'span',
+		},
+	});
+
+	type TagProps = VariantProps<typeof tag>;
 </script>
 
 <script lang="ts" setup>
-import { computed } from "vue";
+	import { computed } from 'vue';
 
-const props = defineProps<{
-  color?: TagProps["color"];
-  href?: string;
-}>();
+	const props = defineProps<{
+		color?: TagProps['color'];
+		href?: string;
+	}>();
 
-const spanOrLink = computed(() => {
-  return props.href ? "a" : "span";
-});
+	const spanOrLink = computed(() => {
+		return props.href ? 'a' : 'span';
+	});
 </script>
 
 <template>
-  <Component
-    v-bind="$attrs"
-    :is="spanOrLink"
-    :class="
-      tag({
-        color: props.color,
-      })
-    "
-  >
-    <slot />
-  </Component>
+	<Component
+		v-bind="$attrs"
+		:is="spanOrLink"
+		:class="
+			tag({
+				color: props.color,
+				type: props.href ? 'link' : 'span',
+			})
+		"
+	>
+		<slot />
+	</Component>
 </template>
-
-<style scoped>
-a.tag {
-  cursor: pointer;
-
-  &:hover {
-    box-shadow: var(--shadow-tiny);
-  }
-}
-
-.tag {
-  --bg-color: var(--white);
-  background-color: var(--bg-color);
-  border-radius: var(--rounded-sm);
-  border: 1px solid var(--gray-800);
-  color: var(--gray-800);
-  font-size: var(--text-utility);
-  letter-spacing: 0.05em;
-  line-height: 1;
-  padding: 0.375rem 0.5rem;
-  text-decoration: none;
-  text-transform: uppercase;
-  transition: box-shadow 200ms ease-in-out;
-  will-change: box-shadow;
-
-  &.cyan {
-    --bg-color: var(--cyan-300);
-  }
-
-  &.violet {
-    --bg-color: var(--violet-300);
-  }
-
-  &.yellow {
-    --bg-color: var(--yellow-300);
-  }
-
-  &.red {
-    --bg-color: var(--red-300);
-  }
-
-  &.lime {
-    --bg-color: var(--lime-300);
-  }
-}
-</style>
