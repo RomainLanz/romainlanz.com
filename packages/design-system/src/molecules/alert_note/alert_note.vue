e1869566cbe6ade3db07e6724d5a78eb04da5597
Romain Lanz <romain.lanz@pm.me>
2024-09-08 00:31:45 +0200

    refactor(design-system:alert_note): use unocss

--- packages/design-system/src/molecules/alert_note/alert_note.vue
+++ packages/design-system/src/molecules/alert_note/alert_note.vue
@@ -1,98 +1,62 @@
 <script lang="ts">
-import { cva, type VariantProps } from "class-variance-authority";
+	import { tv, type VariantProps } from 'tailwind-variants';
 
-const alert = cva("alert", {
-  variants: {
-    type: {
-      info: "info",
-      success: "success",
-      warning: "warning",
-      danger: "danger",
-    },
-  },
-});
+	const alert = tv({
+		slots: {
+			base: 'flex items-start bg-transparent rounded-lg border-2 border-solid border-gray-800 text-gray-800 gap-2 py-3 px-4',
+			icon: 'text-gray-600 shrink-0 size-[20px] pt-[2px]',
+		},
 
-const alertIconMap = {
-  info: "info",
-  success: "success",
-  warning: "danger",
-  danger: "close-large",
-} as const;
+		variants: {
+			type: {
+				info: {
+					base: 'bg-cyan-100 border-cyan-800 text-cyan-800',
+					icon: 'text-cyan-600',
+				},
+				success: {
+					base: 'bg-lime-100 border-lime-800 text-lime-800',
+					icon: 'text-lime-600',
+				},
+				warning: {
+					base: 'bg-yellow-200 border-yellow-800 text-yellow-800',
+					icon: 'text-yellow-600',
+				},
+				danger: {
+					base: 'bg-red-100 border-red-800 text-red-800',
+					icon: 'text-red-600',
+				},
+			},
+		},
+	});
 
-type AlertProps = VariantProps<typeof alert>;
+	const alertIconMap = {
+		info: 'info',
+		success: 'success',
+		warning: 'danger',
+		danger: 'close-large',
+	} as const;
+
+	type AlertProps = VariantProps<typeof alert>;
 </script>
 
 <script lang="ts" setup>
-import Icon from "../../atoms/icon/icon.vue";
-import { computed } from "vue";
+	import Icon from '../../atoms/icon/icon.vue';
+	import { computed } from 'vue';
 
-const props = defineProps<{
-  type: AlertProps["type"];
-}>();
+	const props = defineProps<{
+		type: NonNullable<AlertProps['type']>;
+	}>();
 
-const iconName = computed(() => alertIconMap[props.type]);
+	const iconName = computed(() => alertIconMap[props.type]);
+	const { base, icon } = alert({ type: props.type });
 </script>
 
 <template>
-  <div :class="alert({ type })" role="alert">
-    <div class="icon">
-      <Icon :name="iconName" />
-    </div>
+	<div :class="base()" role="alert">
+		<div :class="icon()">
+			<Icon :name="iconName" />
+		</div>
 
-    <slot />
-  </div>
+		<slot />
+	</div>
 </template>
-
-<style scoped>
-.alert {
-  --bg-color: transparent;
-  --border-color: var(--gray-800);
-  --text-color: var(--gray-800);
-  --icon-color: var(--gray-600);
-
-  align-items: flex-start;
-  background-color: var(--bg-color);
-  border-radius: var(--rounded);
-  border: 2px solid var(--border-color);
-  color: var(--text-color);
-  display: flex;
-  gap: calc(2 * var(--space));
-  padding: calc(3 * var(--space)) calc(4 * var(--space));
-
-  .icon {
-    color: var(--icon-color);
-    flex-shrink: 0;
-    height: 20px;
-    padding-top: 2px;
-    width: 20px;
-  }
-
-  &.info {
-    --bg-color: var(--cyan-100);
-    --border-color: var(--cyan-800);
-    --text-color: var(--cyan-800);
-    --icon-color: var(--cyan-600);
-  }
-
-  &.success {
-    --bg-color: var(--lime-100);
-    --border-color: var(--lime-800);
-    --text-color: var(--lime-800);
-    --icon-color: var(--lime-600);
-  }
-
-  &.warning {
-    --bg-color: var(--yellow-200);
-    --border-color: var(--yellow-800);
-    --text-color: var(--yellow-800);
-    --icon-color: var(--yellow-600);
-  }
-
-  &.danger {
-    --bg-color: var(--red-100);
-    --border-color: var(--red-800);
-    --text-color: var(--red-800);
-    --icon-color: var(--red-600);
-  }
-}
-</style>
