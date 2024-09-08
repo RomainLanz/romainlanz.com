1e51a8cc131271840605911b63edabcb9e8dee59
Romain Lanz <romain.lanz@pm.me>
2024-09-08 11:49:09 +0200

    feat(design-system:field): make the component

--- packages/design-system/src/molecules/field/field.stories.ts
+++ packages/design-system/src/molecules/field/field.stories.ts
@@ -1,25 +1,42 @@
-import Field from "./field.vue";
-import type { Meta, StoryObj } from "@storybook/vue3";
+import Field from './field.vue';
+import type { Meta, StoryObj } from '@storybook/vue3';
 
 const meta = {
-  component: Field,
-  title: "Molecules/Field",
+	component: Field,
+	title: 'Molecules/Field',
+	argTypes: {
+		label: {
+			type: 'string',
+		},
+		errorMessage: {
+			type: 'string',
+		},
+		helpMessage: {
+			type: 'string',
+		},
+	},
 } satisfies Meta<typeof Field>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Base: Story = {
-  argTypes: {
-    label: {
-      type: "string",
-    },
-    errorMessage: {
-      type: "string",
-    },
-    helpMessage: {
-      type: "string",
-    },
-  },
+export const Base: Story = {};
+
+export const WithLabel: Story = {
+	args: {
+		label: 'Label',
+	},
+};
+
+export const WithErrorMessage: Story = {
+	args: {
+		errorMessage: 'Error message',
+	},
+};
+
+export const WithHelpMessage: Story = {
+	args: {
+		helpMessage: 'Help message',
+	},
 };
--- packages/design-system/src/molecules/field/field.vue
+++ packages/design-system/src/molecules/field/field.vue
@@ -1,57 +1,29 @@
 <script lang="ts" setup>
-defineProps<{
-  label?: string;
-  errorMessage?: string;
-  helpMessage?: string;
-}>();
+	import { FieldRoot, FieldLabel, FieldInput, FieldHelperText, FieldErrorText } from '@ark-ui/vue';
+
+	defineProps<{
+		label?: string;
+		errorMessage?: string;
+		helpMessage?: string;
+	}>();
 </script>
 
 <template>
-  <div class="form_group">
-    <label v-if="label" class="form_label" for="">{{ label }}</label>
+	<FieldRoot class="flex flex-col gap-1" :invalid="typeof errorMessage !== 'undefined'">
+		<FieldLabel v-if="label" class="text-gray-800">
+			{{ label }}
+		</FieldLabel>
 
-    <!--TODO: Create dedicated component -->
+		<FieldInput
+			class="border-2 border-solid border-gray-800 rounded-lg bg-transparent px-4 py-2 disabled:cursor-not-allowd placeholder:text-gray-600 placeholder:font-bold"
+		/>
 
-    <span v-if="errorMessage" class="form_error">{{ errorMessage }}</span>
-    <span v-else-if="helpMessage" class="form_help">{{ helpMessage }}</span>
-  </div>
+		<FieldErrorText class="text-red-500 text-sm uppercase">
+			{{ errorMessage }}
+		</FieldErrorText>
+
+		<FieldHelperText v-if="!errorMessage" class="text-cyan-500 text-sm">
+			{{ helpMessage }}
+		</FieldHelperText>
+	</FieldRoot>
 </template>
-
-<style scoped>
-.form_group {
-  display: flex;
-  flex-direction: column;
-  gap: 0.25rem;
-}
-
-.form_label {
-  color: var(--gray-800);
-}
-
-.form_control {
-  border: 2px solid var(--gray-800);
-  border-radius: var(--rounded);
-  background: transparent;
-  padding: 0.5rem 1rem;
-
-  &[disabled] {
-    cursor: not-allowed;
-  }
-
-  &::placeholder {
-    color: var(--gray-600);
-    font-weight: var(--text-bold);
-  }
-}
-
-.form_group .form_error {
-  color: var(--red-500);
-  font-size: var(--text-sm);
-  text-transform: uppercase;
-}
-
-.form_group .form_help {
-  color: var(--cyan-500);
-  font-size: var(--text-sm);
-}
-</style>
