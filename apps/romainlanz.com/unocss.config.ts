47bd913642d63afee14393e067158ebdbe22a323
Romain Lanz <romain.lanz@pm.me>
2024-10-20 22:03:49 +0200

    refactor(site): use new colors export for unocss token

--- apps/romainlanz.com/unocss.config.ts
+++ apps/romainlanz.com/unocss.config.ts
@@ -1,9 +1,9 @@
-import { tokens } from '@rlanz/design-system';
+import { colors } from '@rlanz/design-system/tokens';
 import defineConfig from '@rlanz/tooling/unocss';
 
 export default defineConfig({
 	theme: {
-		colors: tokens,
+		colors,
 	},
 	content: {
 		filesystem: ['./inertia/**/*.vue', './node_modules/@rlanz/design-system/**/*.vue'],
