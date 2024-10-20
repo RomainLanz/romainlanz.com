2df47e969898dce12941e06de3c5fcba7783a680
Romain Lanz <romain.lanz@pm.me>
2024-10-20 22:07:48 +0200

    refactor(design-system): remove unneeded build step

--- packages/design-system/vite.config.ts
+++ packages/design-system/vite.config.ts
@@ -1,26 +1,7 @@
 import { defineConfig } from 'vite';
 import UnoCSS from 'unocss/vite';
 import Vue from '@vitejs/plugin-vue';
-import DTS from 'vite-plugin-dts';
 
 export default defineConfig({
-	plugins: [
-		UnoCSS(),
-		Vue(),
-		DTS({
-			cleanVueFileName: true,
-			exclude: ['src/**/*.stories.ts'],
-			tsconfigPath: 'tsconfig.app.json',
-		}),
-	],
-	build: {
-		outDir: 'build',
-		lib: {
-			entry: 'src/main.ts',
-			formats: ['es'],
-		},
-		rollupOptions: {
-			external: ['vue', '@inertiajs/vue3', 'tailwind-variants'],
-		},
-	},
+	plugins: [UnoCSS(), Vue()],
 });
