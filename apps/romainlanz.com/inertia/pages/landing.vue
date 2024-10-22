6e298aefff40f78cd6c5115deaaf4d6d6e5a29bd
Romain Lanz <romain.lanz@pm.me>
2024-10-22 22:16:06 +0200

    feats(site:landing): add last articles

--- apps/romainlanz.com/inertia/pages/landing.vue
+++ apps/romainlanz.com/inertia/pages/landing.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
 	import { Head } from '@inertiajs/vue3';
+	import ArticleCard from '@rlanz/design-system/article-card';
 	import Hero from '@rlanz/design-system/hero';
 	import Link from '@rlanz/design-system/link';
+	import { client } from '@rlanz/rpc/client';
+	import { computed } from 'vue';
+
+	defineProps<{
+		articles: any[];
+	}>();
+
+	const allArticlesUrl = computed(() => {
+		return client.$url('articles.index');
+	});
 </script>
 
 <template>
@@ -13,11 +24,19 @@
 		<div class="flex items-baseline justify-between">
 			<h3 class="text-5xl text-gray-800">Mes derniers articles</h3>
 
-			<Link href="/articles">Tous</Link>
+			<Link :href="allArticlesUrl" prefetch>Tous</Link>
 		</div>
 
 		<div class="grid cols-2 gap-6">
-			<!-- Articles -->
+			<ArticleCard
+				v-for="article in articles"
+				:key="article.id"
+				:title="article.title"
+				:slug="article.slug"
+				:date="article.published_at"
+				:excerpt="article.description"
+				:reading-time="5"
+			/>
 		</div>
 	</section>
 </template>
