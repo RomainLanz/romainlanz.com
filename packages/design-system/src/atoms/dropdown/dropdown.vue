235f556e509bd65b378c1147ab19340937591cca
Romain Lanz <romain.lanz@pm.me>
2024-10-25 00:07:39 +0200

    fix(design-system:dropdown): make the content of dropdown menu client only

--- packages/design-system/src/atoms/dropdown/dropdown.vue
+++ packages/design-system/src/atoms/dropdown/dropdown.vue
@@ -11,6 +11,7 @@
 <script lang="ts" setup>
 	import { router } from '@inertiajs/vue3';
 	import { MenuContent, MenuItem, MenuPositioner, MenuRoot, MenuTrigger } from '@ark-ui/vue';
+	import ClientOnly from '../client_only/client_only.ts';
 
 	const { actions } = defineProps<{
 		actions: MenuAction[];
@@ -33,22 +34,24 @@
 			<slot />
 		</MenuTrigger>
 
-		<Teleport to="body">
-			<MenuPositioner>
-				<MenuContent class="bg-white min-w-48 rounded-lg p-2 border-2 border-solid border-gray-800 shadow-small">
-					<template v-for="action in actions">
-						<MenuItem
-							v-if="action.enabled !== false"
-							class="cursor-pointer px-2 py-0.5 rounded-ms hover:bg-yellow-300 transition-colors duration-200"
-							:id="`${action.code}-menu-action`"
-							:code="action.code"
-							:value="action.code"
-						>
-							{{ action.label }}
-						</MenuItem>
-					</template>
-				</MenuContent>
-			</MenuPositioner>
-		</Teleport>
+		<ClientOnly>
+			<Teleport to="body">
+				<MenuPositioner>
+					<MenuContent class="bg-white min-w-48 rounded-lg p-2 border-2 border-solid border-gray-800 shadow-small">
+						<template v-for="action in actions">
+							<MenuItem
+								v-if="action.enabled !== false"
+								class="cursor-pointer px-2 py-0.5 rounded-ms hover:bg-yellow-300 transition-colors duration-200"
+								:id="`${action.code}-menu-action`"
+								:code="action.code"
+								:value="action.code"
+							>
+								{{ action.label }}
+							</MenuItem>
+						</template>
+					</MenuContent>
+				</MenuPositioner>
+			</Teleport>
+		</ClientOnly>
 	</MenuRoot>
 </template>
