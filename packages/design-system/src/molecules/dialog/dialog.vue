751a4c6409fcb29079cbb6ec0ea8d2cbddc76815
Romain Lanz <romain.lanz@pm.me>
2024-10-24 18:50:26 +0200

    fix(design-system:dialog): make the content render only on the client

--- packages/design-system/src/molecules/dialog/dialog.vue
+++ packages/design-system/src/molecules/dialog/dialog.vue
@@ -10,37 +10,48 @@
 	} from '@ark-ui/vue/dialog';
 	import Icon from '../../atoms/icon/icon.vue';
 	import Panel from '../../atoms/panel/panel.vue';
+	import ClientOnly from '../../atoms/client_only/client_only.ts';
 
-	const { backdrop = true } = defineProps<{
+	const {
+		backdrop = true,
+		lazyMount = true,
+		unmountOnExit = true,
+	} = defineProps<{
 		title: string;
 		backdrop?: boolean;
+		lazyMount?: boolean;
+		unmountOnExit?: boolean;
 	}>();
 </script>
 
 <template>
-	<DialogRoot>
+	<DialogRoot :lazy-mount="lazyMount" :unmount-on-exit="unmountOnExit">
 		<DialogTrigger as-child>
 			<slot name="trigger" />
 		</DialogTrigger>
 
-		<Teleport to="body">
-			<DialogBackdrop v-if="backdrop" class="absolute inset-0 bg-dark opacity-50 z-10" />
+		<ClientOnly>
+			<Teleport to="body">
+				<DialogBackdrop v-if="backdrop" class="absolute inset-0 bg-dark opacity-50" />
 
-			<DialogPositioner class="fixed flex items-center justify-center h-full w-full z-20">
-				<DialogContent as-child>
-					<Panel class="bg-white max-w-2xl w-full">
-						<div class="flex items-start justify-between">
-							<DialogTitle class="text-4xl">{{ title }}</DialogTitle>
+				<DialogPositioner class="fixed inset-0 w-screen overflow-y-auto">
+					<div class="flex min-h-full items-end justify-center p-4 sm:items-center sm:p-0">
+						<DialogContent as-child>
+							<Panel class="bg-white max-w-2xl w-full">
+								<div class="flex items-start justify-between">
+									<DialogTitle class="text-4xl">{{ title }}</DialogTitle>
 
-							<DialogCloseTrigger as-child>
-								<Icon name="close-large" class="cursor-pointer" />
-							</DialogCloseTrigger>
-						</div>
+									<DialogCloseTrigger as-child>
+										<Icon name="close-large" class="cursor-pointer" />
+									</DialogCloseTrigger>
+								</div>
 
-						<slot name="content" />
-					</Panel>
-				</DialogContent>
-			</DialogPositioner>
-		</Teleport>
+								<slot name="content" />
+							</Panel>
+						</DialogContent>
+					</div>
+				</DialogPositioner>
+			</Teleport>
+		</ClientOnly>
 	</DialogRoot>
 </template>
