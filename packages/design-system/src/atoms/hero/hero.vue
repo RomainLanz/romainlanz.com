59b448399e8c28cbb34b5cce7e42133f0d583ca6
Romain Lanz <romain.lanz@pm.me>
2024-11-10 00:25:58 +0100

    feat(design-system:hero): make it responsive

--- packages/design-system/src/atoms/hero/hero.vue
+++ packages/design-system/src/atoms/hero/hero.vue
@@ -7,9 +7,9 @@
 	<section class="max-w-7xl px-4 my-38 m-auto">
 		<div class="flex">
 			<div class="flex flex-col gap-6">
-				<h1 class="text-5xl">
-					Apprenez le <Highlight color="red.300">JavaScript</Highlight> <br />
-					et <Highlight color="lime.300" :rotation="2">Node.js</Highlight> à travers différents articles et vidéos
+				<h1 class="text-4xl sm:text-6xl lg:text-5xl">
+					Apprenez le <Highlight color="red.300">JavaScript</Highlight> et
+					<Highlight color="lime.300" :rotation="2">Node.js</Highlight> à travers différents articles et vidéos
 				</h1>
 
 				<div class="flex gap-4">
@@ -17,31 +17,7 @@
 				</div>
 			</div>
 
-			<img class="mt-[10px]" :src="heroImage" alt="" />
+			<img class="hidden lg:block mt-[10px]" :src="heroImage" alt="" />
 		</div>
 	</section>
 </template>
-
-<style module>
-	.funny {
-		position: relative;
-		display: inline-block;
-
-		&:before {
-			background-color: theme('colors.red.300');
-			content: '';
-			height: 95%;
-			margin-left: -2.5%;
-			position: absolute;
-			transform: rotate(-2deg);
-			transition: transform 300ms ease-in-out;
-			width: 105%;
-			will-change: transform;
-			z-index: -1;
-		}
-
-		&:hover:before {
-			transform: rotate(0deg);
-		}
-	}
-</style>
