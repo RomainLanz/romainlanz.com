aa0fb232f422c18feb5dd6beddd519f2644f08f1
Romain Lanz <romain.lanz@pm.me>
2024-11-09 23:09:43 +0100

    fix(site:articles): do not take into account not published article

--- apps/romainlanz.com/app/articles/repositories/article_repository.ts
+++ apps/romainlanz.com/app/articles/repositories/article_repository.ts
@@ -50,6 +50,8 @@ export class ArticleRepository {
 		const countQueryResult = await db
 			.selectFrom('articles')
 			.select((builder) => builder.fn.count('articles.id').as('count'))
+			.where('published_at', 'is not', null)
+			.where('published_at', '<=', new Date())
 			.execute();
 
 		return Number(countQueryResult[0].count);
