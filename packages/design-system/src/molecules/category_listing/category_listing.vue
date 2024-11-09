760f7e1aba7bf9719a3e207faf8e5d66774bd12a
Romain Lanz <romain.lanz@pm.me>
2024-11-09 22:56:20 +0100

    feat(design-system:category_listing): add all category

--- packages/design-system/src/molecules/category_listing/category_listing.vue
+++ packages/design-system/src/molecules/category_listing/category_listing.vue
@@ -3,6 +3,7 @@
 	import Illustration, { type IllustrationName } from '../../atoms/illustration/illustration.vue';
 
 	defineProps<{
+		allArticlesCount: number;
 		categories: Array<{
 			id: string;
 			name: string;
@@ -19,6 +20,22 @@
 
 <template>
 	<ul class="list-none m-0 p-0">
+		<li class="border-solid border-2 border-transparent w-[300px] rounded-lg hover:bg-yellow-300 hover:border-gray-800">
+			<Link class="flex items-center text-lg px-4 py-3 gap-3" href="#">
+				<div class="grid place-items-center w-10">
+					<Illustration class="h-10" name="all" />
+				</div>
+
+				<span>All</span>
+
+				<span
+					class="bg-yellow-100 text-xs rounded-md border border-solid border-gray-800 grid place-items-center h-[20px] px-1 min-w-[20px]"
+				>
+					{{ allArticlesCount }}
+				</span>
+			</Link>
+		</li>
+
 		<li
 			class="border-solid border-2 border-transparent w-[300px] rounded-lg hover:bg-yellow-300 hover:border-gray-800"
 			v-for="category in categories"
