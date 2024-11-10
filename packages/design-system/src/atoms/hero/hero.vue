d6305a5a4459073e793bcfdd663ac331ccf82766
Romain Lanz <romain.lanz@pm.me>
2024-11-10 15:32:27 +0100

    chore(design-system:hero): add subtitle

--- packages/design-system/src/atoms/hero/hero.vue
+++ packages/design-system/src/atoms/hero/hero.vue
@@ -8,9 +8,15 @@
 		<div class="flex">
 			<div class="flex flex-col gap-6">
 				<h1 class="text-4xl sm:text-6xl lg:text-5xl">
-					Apprenez le <Highlight color="red.300">JavaScript</Highlight> et
+					Apprenez le <Highlight color="red.300">JavaScript</Highlight>,
+					<Highlight color="cyan.300">TypeScript</Highlight> et
 					<Highlight color="lime.300" :rotation="2">Node.js</Highlight> à travers différents articles et vidéos
 				</h1>
+
+				<p class="text-xl">
+					Je suis Romain Lanz, développeur full-stack avec plus de 10 ans d'expérience. Sur ce site, je partage mes
+					connaissances et mes techniques pour vous aider à progresser.
+				</p>
 
 				<div class="flex gap-4">
 					<slot name="actions" />
