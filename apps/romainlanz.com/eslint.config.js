72500320369660f386583dd367a0c77b1ca08024
Romain Lanz <romain.lanz@pm.me>
2024-11-10 23:24:05 +0100

    feat(tooling:eslint): add sort import

--- apps/romainlanz.com/eslint.config.js
+++ apps/romainlanz.com/eslint.config.js
@@ -1,7 +1,23 @@
 import { julr } from '@julr/tooling-configs/eslint';
 
-export default julr({
-	typescript: {
-		tsconfigPath: ['./tsconfig.json', './inertia/tsconfig.json'],
+export default julr(
+	{
+		typescript: {
+			tsconfigPath: ['./tsconfig.json', './inertia/tsconfig.json'],
+		},
 	},
-});
+	{
+		rules: {
+			'perfectionist/sort-imports': [
+				'error',
+				{
+					'groups': ['side-effect', 'builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'type'],
+					'internal-pattern': ['#**/*', '~/**/*'],
+					'newlines-between': 'never',
+					'order': 'asc',
+					'type': 'alphabetical',
+				},
+			],
+		},
+	}
+);
