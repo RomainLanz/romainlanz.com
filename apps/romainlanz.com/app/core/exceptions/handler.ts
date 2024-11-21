8da5ac89e12ebd5c5d876cdc5d23648a0b27031d
Romain Lanz <romain.lanz@pm.me>
2024-11-21 22:36:17 +0100

    chore: re-enable error page

--- apps/romainlanz.com/app/core/exceptions/handler.ts
+++ apps/romainlanz.com/app/core/exceptions/handler.ts
@@ -8,14 +8,14 @@ export default class HttpExceptionHandler extends ExceptionHandler {
 	 * In debug mode, the exception handler will display verbose errors
 	 * with pretty printed stack traces.
 	 */
-	protected debug = app.inProduction;
+	protected debug = !app.inProduction;
 
 	/**
 	 * Status pages are used to display a custom HTML pages for certain error
 	 * codes. You might want to enable them in production only, but feel
 	 * free to enable them in development as well.
 	 */
-	protected renderStatusPages = !app.inProduction;
+	protected renderStatusPages = app.inProduction;
 
 	/**
 	 * Status pages is a collection of error code range and a callback
