a299a44fe6445242f7a4257ae6b55929c86c965d
Romain Lanz <romain.lanz@pm.me>
2024-12-08 23:06:23 +0100

    fix: use env variable for base app url

--- apps/romainlanz.com/start/env.ts
+++ apps/romainlanz.com/start/env.ts
@@ -14,6 +14,7 @@ import { Env } from '@adonisjs/core/env';
 export default await Env.create(new URL('../', import.meta.url), {
 	NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
 	PORT: Env.schema.number(),
+	APP_URL: Env.schema.string(),
 	APP_KEY: Env.schema.string(),
 	HOST: Env.schema.string({ format: 'host' }),
 	LOG_LEVEL: Env.schema.string(),
--- packages/rpc/src/client.ts
+++ packages/rpc/src/client.ts
@@ -3,5 +3,5 @@ import { api } from "@rlanz/site/api";
 
 export const client = createTuyau({
   api,
-  baseUrl: "http://romainlanz.localhost:3333",
+  baseUrl: globalThis.RomainLanz.appUrl,
 });
