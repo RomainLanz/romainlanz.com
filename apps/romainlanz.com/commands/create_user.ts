5ab417edbc4b09beb7e627688d44baafe3e2f021
Romain Lanz <romain.lanz@pm.me>
2024-12-14 23:41:38 +0100

    fix(core): cast user role into number when creating user

--- apps/romainlanz.com/commands/create_user.ts
+++ apps/romainlanz.com/commands/create_user.ts
@@ -37,7 +37,7 @@ export default class CreateUser extends BaseCommand {
 				created_at: new Date(),
 				name,
 				email,
-				role,
+				role: Number(role),
 				password: hashedPassword,
 			})
 			.execute();
