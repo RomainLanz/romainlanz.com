d78eb1aa9a7e39030a05aeaafd7de8aa6f6872d4
Romain Lanz <romain.lanz@pm.me>
2024-12-15 00:17:00 +0100

    feat(core): display error when deferring

--- apps/romainlanz.com/app/core/services/defer.ts
+++ apps/romainlanz.com/app/core/services/defer.ts
@@ -1,3 +1,8 @@
+import logger from '@adonisjs/core/services/logger';
 import { DeferQueue } from '@poppinss/defer';
 
 export const defer = new DeferQueue();
+
+defer.onError = (error) => {
+	logger.error(error);
+};
