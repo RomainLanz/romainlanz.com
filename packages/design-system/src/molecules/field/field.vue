7456e162984df08d173f0a22c4ae7fd043325d43
Romain Lanz <romain.lanz@pm.me>
2024-12-21 00:09:35 +0100

    fix(design-system:field): dynamically set the component

--- packages/design-system/src/molecules/field/field.vue
+++ packages/design-system/src/molecules/field/field.vue
@@ -20,6 +20,7 @@
 		helpMessage?: string;
 	}>();
 
+	const inputComponent = computed(() => (type === 'textarea' ? FieldTextarea : FieldInput));
 	const invalid = computed(() => typeof errorMessage !== 'undefined');
 </script>
 
@@ -29,24 +30,14 @@
 			{{ label }}
 		</FieldLabel>
 
-		<!-- Mark as Client Only while figuring out how to make it work in SSR -->
-		<ClientOnly>
-			<FieldTextarea
-				v-if="type === 'textarea'"
-				v-bind="$attrs"
-				v-model="model"
-				class="border-2 border-solid border-gray-800 resize-y rounded-lg bg-transparent px-4 py-2 disabled:cursor-not-allowed placeholder:text-gray-600 placeholder:font-bold"
-				:autoresize="true"
-			/>
-
-			<FieldInput
-				v-else
-				v-bind="$attrs"
-				v-model="model"
-				class="border-2 border-solid border-gray-800 rounded-lg bg-transparent px-4 py-2 disabled:cursor-not-allowed placeholder:text-gray-600 placeholder:font-bold"
-				:type
-			/>
-		</ClientOnly>
+		<Component
+			:is="inputComponent"
+			v-bind="$attrs"
+			v-model="model"
+			class="border-2 border-solid border-gray-800 resize-y rounded-lg bg-transparent px-4 py-2 disabled:cursor-not-allowed placeholder:text-gray-600 placeholder:font-bold"
+			:autoresize="true"
+			:type
+		/>
 
 		<FieldErrorText class="text-red-500 text-sm uppercase">
 			{{ errorMessage }}
