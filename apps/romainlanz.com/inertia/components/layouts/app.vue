3abd92ea67f0496aee4de43f19052de256f89d24
Romain Lanz <romain.lanz@pm.me>
2025-01-02 19:57:16 +0100

    style: remove unused code

--- apps/romainlanz.com/inertia/components/layouts/app.vue
+++ apps/romainlanz.com/inertia/components/layouts/app.vue
@@ -1,23 +1,20 @@
 <script lang="ts" setup>
-	import { useForm } from '@inertiajs/vue3';
 	import Footer from '@rlanz/design-system/footer';
-	import NewsletterForm from '@rlanz/design-system/newsletter-form';
 	import TopBar from '@rlanz/design-system/top-bar';
-	import { ref } from 'vue';
 	import LoginDialog from '~/components/auth/login_dialog.vue';
 	import LoadFragment from '~/components/load_fragment.vue';
 	import { useCurrentUser } from '~/composables/use_current_user';
 
 	const currentUser = useCurrentUser();
 
-	const openNewsletterDialog = ref(false);
-	const form = useForm({
-		email: '',
-	});
-
-	function onRegister() {
-		console.log(form);
-	}
+	// const openNewsletterDialog = ref(false);
+	// const form = useForm({
+	// 	email: '',
+	// });
+	//
+	// function onRegister() {
+	// 	console.log(form);
+	// }
 </script>
 
 <template>
