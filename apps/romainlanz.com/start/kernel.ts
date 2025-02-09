a58913a76f01b448db9a065dea91f796c9c56be9
Romain Lanz <romain.lanz@pm.me>
2025-02-09 10:51:04 +0100

    refactor: remove error_bag_middleware.ts

--- apps/romainlanz.com/start/kernel.ts
+++ apps/romainlanz.com/start/kernel.ts
@@ -44,7 +44,6 @@ router.use([
 	() => import('@adonisjs/shield/shield_middleware'),
 	() => import('#core/middleware/detect_user_locale_middleware'),
 	() => import('#core/middleware/initialize_bouncer_middleware'),
-	() => import('#core/middleware/error_bag_middleware'),
 ]);
 
 /**
