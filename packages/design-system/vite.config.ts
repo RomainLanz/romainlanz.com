f9993133b77431948d37ac077d994e9d806547c4
Romain Lanz <romain.lanz@pm.me>
2025-05-18 20:58:44 +0200

    feat(design-system): allow to write story in JSX

--- packages/design-system/vite.config.ts
+++ packages/design-system/vite.config.ts
@@ -1,7 +1,8 @@
 import Vue from '@vitejs/plugin-vue';
+import VueJSX from '@vitejs/plugin-vue-jsx';
 import UnoCSS from 'unocss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [UnoCSS(), Vue()],
+	plugins: [UnoCSS(), Vue(), VueJSX()],
 });
