7d7a0eb335252350f7679b01cbd7a61c0e13944a
Romain Lanz <romain.lanz@pm.me>
2025-06-08 20:48:42 +0200

    chore: update dependencies

--- packages/design-system/.storybook/main.ts
+++ packages/design-system/.storybook/main.ts
@@ -2,13 +2,7 @@ import type { StorybookConfig } from '@storybook/vue3-vite';
 
 const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-	addons: [
-		'@storybook/addon-onboarding',
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@chromatic-com/storybook',
-		'@storybook/addon-interactions',
-	],
+	addons: ['storybook/actions', '@chromatic-com/storybook'],
 	framework: {
 		name: '@storybook/vue3-vite',
 		options: {},
